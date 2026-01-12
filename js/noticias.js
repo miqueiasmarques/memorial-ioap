0b6e2f49f46996dd68733581919522c6d1bad23e
miqueiasmarques <miqueiasmarques@gmail.com>
2026-01-12 07:05:22 -0300

    Add files via upload

---
+++ js/noticias.js
@@ -0,0 +1,11 @@
+const baseNoticias = [
+    {
+        id: 1,
+        titulo: "Exposição celebra 80 anos de Atos Oficiais na 53ª Expofeira",
+        resumo: "A Imprensa Oficial do Amapá apresenta acervo histórico, registros e máquinas antigas na maior feira de negócios da Amazônia.",
+        texto: "Além de garantir a autenticidade dos atos oficiais do Governo do Estado, a Imprensa Oficial do Amapá presta um importante serviço à sociedade no que se refere à democratização da informação. A exposição 'Memórias Oficiais do Amapá: 80 anos de Atos Oficiais' conta um pouco da trajetória dos 60 anos de criação do Diário Oficial, com edições de 1945 e momentos marcantes da história amapaense.",
+        data: "12/01/2026",
+        // Definindo a ordem: a primeira é a principal
+        imagens: ["img/expo1.jpg", "img/expo2.jpg", "img/expo3.jpg"] 
+    }
+];
