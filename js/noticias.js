50d6f4c2c927450ca7f424eb9b8bd86e46187661
miqueiasmarques <miqueiasmarques@gmail.com>
2026-01-13 02:57:28 -0300

    Add files via upload

--- js/noticias.js
+++ js/noticias.js
@@ -1,11 +1,19 @@
 const baseNoticias = [
     {
+        id: 2,
+        titulo: "Estudantes visitam exposição 'Memórias Oficiais do Amapá'",
+        resumo: "Alunos da rede estadual e universitários participaram de uma imersão histórica guiada pelo Núcleo de Imprensa Oficial (NIO).",
+        texto: "A exposição 'Memórias Oficiais do Amapá: 80 anos de Atos Oficiais', organizada pelo Governo do Estado, recebeu visitas ilustres de estudantes da rede estadual e acadêmicos universitários. <br><br> Durante a visita, guiada pela equipe do Núcleo de Imprensa Oficial (NIO), os alunos puderam conhecer de perto as máquinas antigas de tipografia e as edições originais dos jornais que registraram a transformação do Território em Estado. A iniciativa busca fortalecer a identidade cultural e a preservação da memória administrativa do Amapá. <br><br> <em>Fonte e Créditos: Portal do Governo do Amapá (SEMA).</em>",
+        data: "13/01/2026",
+        // Certifique-se de salvar as fotos na pasta img com esses nomes:
+        imagens: ["img/visita-estudantes-1.jpg", "img/visita-estudantes-2.jpg", "img/visita-estudantes-3.jpg"] 
+    },
+    {
         id: 1,
         titulo: "Exposição celebra 80 anos de Atos Oficiais na 53ª Expofeira",
         resumo: "A Imprensa Oficial do Amapá apresenta acervo histórico, registros e máquinas antigas na maior feira de negócios da Amazônia.",
-        texto: "Além de garantir a autenticidade dos atos oficiais do Governo do Estado, a Imprensa Oficial do Amapá presta um importante serviço à sociedade no que se refere à democratização da informação. A exposição 'Memórias Oficiais do Amapá: 80 anos de Atos Oficiais' conta um pouco da trajetória dos 60 anos de criação do Diário Oficial, com edições de 1945 e momentos marcantes da história amapaense.",
+        texto: "Além de garantir a autenticidade dos atos oficiais do Governo do Estado, a Imprensa Oficial do Amapá presta um importante serviço à sociedade no que se refere à democratização da informação. A exposição 'Memórias Oficiais do Amapá' conta um pouco da trajetória de criação do Diário Oficial, com edições de 1945 e momentos marcantes da história amapaense.",
         data: "12/01/2026",
-        // Definindo a ordem: a primeira é a principal
         imagens: ["img/expo1.jpg", "img/expo2.jpg", "img/expo3.jpg"] 
     }
 ];
